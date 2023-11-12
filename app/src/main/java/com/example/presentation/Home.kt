2b6d842e0ef6210d85f5820b62e551255768a5d1
vi-the <vingo20@student.sdu.dk>
2023-11-12 22:04:58 +0100

    route between pages

---
+++ app/src/main/java/com/example/presentation/Home.kt
@@ -0,0 +1,12 @@
+package com.example.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weshare20.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/presentation/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weshare20.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
