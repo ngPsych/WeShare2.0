7e31d94afa313af61171afdbe7d33f35d8850216
Harun <harunabdi8@gmail.com>
2023-11-13 15:47:13 +0100

    db connection

--- app/src/main/java/com/example/presentation/signUp.kt
+++ app/src/main/java/com/example/presentation/signUp.kt
@@ -5,21 +5,29 @@ import android.os.Bundle
 import com.example.weshare20.R
 import android.widget.Button
 import android.widget.EditText
+import com.example.weshare20.persistence.Database
+
 class signUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        val fullnameEditText: EditText = findViewById(R.id.signupFullname)
         val usernameEditText: EditText = findViewById(R.id.signupUsername)
         val emailEditText: EditText = findViewById(R.id.signupEmail)
         val passwordEditText: EditText = findViewById(R.id.signupPassword)
         val signUpButton: Button = findViewById(R.id.signupButton)
 
         signUpButton.setOnClickListener {
+            val fullName = fullnameEditText.text.toString()
             val username = usernameEditText.text.toString()
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
             // Implement your sign-up logic here
+
+
+            val databaseConnector = Database()
+            databaseConnector.insertUser(fullName, username, email, password)
         }
     }
 }
--- app/src/main/java/com/example/weshare20/persistence/Database.kt
+++ app/src/main/java/com/example/weshare20/persistence/Database.kt
@@ -5,29 +5,43 @@ import java.sql.DriverManager
 import java.sql.SQLException
 
 class Database {
-    fun connect() {
-        // Define the database URL, username, and password
-        // postgres://kjcoebww:9UjWwwxCx2jwqrX2RI0Ga2bOygVawfCc@trumpet.db.elephantsql.com/kjcoebww
-        val url = "jdbc:postgres://trumpet.db.elephantsql.com/kjcoebww"
-        val username = "kjcoebww"
-        val password = "9UjWwwxCx2jwqrX2RI0Ga2bOygVawfCc"
+    fun main() {
+        val databaseConnector = Database()
+        val connection = databaseConnector.connect()
 
-        // Attempt to establish a database connection
-        try {
-            // Register the JDBC driver (This step may be optional for some databases)
-            Class.forName("org.postgresql.Driver")
-
-            // Open a connection to the database
-            val connection: Connection = DriverManager.getConnection(url, username, password)
-
-            // Now you have a connection to the database
-
+        connection?.let {
             // Perform database operations here
+            // ...
 
-            // Don't forget to close the connection when you're done
-            connection.close()
-        } catch (e: SQLException) {
-            e.printStackTrace()
+            // Don't forget to close the connection
+            it.close()
         }
     }
+    val jdbcUrl = "jdbc:postgres://trumpet.db.elephantsql.com/kjcoebww"
+    val username = "kjcoebww"
+    val password = "9UjWwwxCx2jwqrX2RI0Ga2bOygVawfCc"
+
+    fun connect(): Connection? {
+        return try {
+            DriverManager.getConnection(jdbcUrl, username, password)
+        } catch (e: SQLException) {
+            e.printStackTrace()
+            null
+        }
+    }
+
+    fun insertUser(fullName: String, username: String, email: String, password: String) {
+        val sql = "INSERT INTO users (fullname, username, email, password) VALUES (?, ?, ?, ?)"
+
+        connect()?.use { conn ->
+            conn.prepareStatement(sql).use { stmt ->
+                stmt.setString(1, fullName)
+                stmt.setString(2, username)
+                stmt.setString(3, email)
+                stmt.setString(4, password)
+                stmt.executeUpdate()
+            }
+        }
+    }
+
 }
