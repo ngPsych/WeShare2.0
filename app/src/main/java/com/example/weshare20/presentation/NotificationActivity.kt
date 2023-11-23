36b466a763f3a71afbfc8b0ea11a9b787485e37b
ngPsych <43138865+ngPsych@users.noreply.github.com>
2023-11-23 13:08:12 +0100

    noti button and page

---
+++ app/src/main/java/com/example/weshare20/presentation/NotificationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weshare20.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weshare20.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification)
+    }
+}
